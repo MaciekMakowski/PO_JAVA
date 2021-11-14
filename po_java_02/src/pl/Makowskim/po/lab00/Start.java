c65815864cfe5934623fe52f8bc2fd203fb03415
MaciekMakowski <73698286+MaciekMakowski@users.noreply.github.com>
2021-11-14 15:38:26 +0100

    poprawa

--- po_java_02/src/pl/Makowskim/po/lab00/Start.java
+++ po_java_02/src/pl/Makowskim/po/lab00/Start.java
@@ -1,6 +1,6 @@
-package pl.Makowskim.po.lab00;
 import java.util.Random;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Start {
 
@@ -17,9 +17,9 @@ public class Start {
         }
     }
     public static void pokaz(int tab[]){
-            for(int i=0;i<tab.length;i++){
-                System.out.println(tab[i]);
-            }
+        for(int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
     }
 
 
@@ -161,30 +161,90 @@ public class Start {
         }
     }
 
+    public static void generujM(int tab[][], int n, int m)
+    {
+        Random r = new Random();
+        for(int i=0; i<tab.length; i++)
+        {
+            for(int j=0; j< tab.length; j++)
+            {
+                tab[i][j] = r.nextInt(20);
+            }
+
+        }
+    }
+
+    public static void wypiszM(int tab[][])
+    {
+        for (int i = 0; i<tab[0].length; i++)
+        {
+            for (int j = 0; j<tab.length; j++)
+            {
+                System.out.print(tab[j][i] + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void mnozenieM(int tab1[][], int tab2[][], int tab3[][])
+    {
+        for(int i=0;i<tab1.length;i++)
+        {
+            for(int j=0;j<tab2.length;j++)
+            {
+                int iloczyn =0;
+                for(int k=0; k<tab3.length; k++)
+                {
+                    iloczyn += tab1[i][k] * tab2[k][j];
+                }
+                tab3[i][j] = iloczyn;
+            }
+        }
+    }
 
 
     public static void main(String[] args){
         int n=5;
-        int[] tab;
-        tab = new int[n];
+        int[] tab1;
+        tab1 = new int[n];
         int minVal = -100;
         int maxVal = 100;
-        generuj(tab,n,minVal,maxVal);
-        pokaz(tab);
+        generuj(tab1,n,minVal,maxVal);
+        pokaz(tab1);
         System.out.println(" ");
-        System.out.println(ileNieparzystch(tab));
-        System.out.println(ileParzystch(tab));
-        System.out.println(ileDodatnich(tab));
-        System.out.println(ileUjemncyh(tab));
-        System.out.println(ileZerowych(tab));
-        System.out.println(ileMaksymalnych(tab));
-        System.out.println(sumaDodatnich(tab));
-        System.out.println(sumaUjemnych(tab));
-        System.out.println(dlugoscMaksymalnegoCiaguDodatnich(tab));
-        odwrocFragment(tab,3,1);
-        pokaz(tab);
-        signum(tab);
-        pokaz(tab);
+        System.out.println(ileNieparzystch(tab1));
+        System.out.println(ileParzystch(tab1));
+        System.out.println(ileDodatnich(tab1));
+        System.out.println(ileUjemncyh(tab1));
+        System.out.println(ileZerowych(tab1));
+        System.out.println(ileMaksymalnych(tab1));
+        System.out.println(sumaDodatnich(tab1));
+        System.out.println(sumaUjemnych(tab1));
+        System.out.println(dlugoscMaksymalnegoCiaguDodatnich(tab1));
+        odwrocFragment(tab1,3,1);
+        pokaz(tab1);
+        signum(tab1);
+        pokaz(tab1);
+
+
+        System.out.println("Podaj liczby m,n,k: ");
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        n = scan.nextInt();
+        int k = scan.nextInt();
+        if(m<1 && m<11 && n<1 && n<11 && k<1 && k<11)
+        {
+            System.out.println("Podaj poprawna wartosc z zakresu [1:10]");
+        }
+        int[][] tab = new int[m][n];
+        generujM(tab,m,n);
+        wypiszM(tab);
+        int[][] tab2 = new int[n][k];
+        generujM(tab2,n,k);
+        wypiszM(tab2);
+        int[][] tab3 = new int[m][k];
+        mnozenieM(tab,tab2,tab3);
+        wypiszM(tab3);
     }
 
 }
