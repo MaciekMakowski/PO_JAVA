57ddef300b2071cae62ee07910ea6a2804774f32
MaciekMakowski <73698286+MaciekMakowski@users.noreply.github.com>
2022-01-06 12:24:31 +0100

    Uzupełnienie labów

---
+++ Lab_08/src/Flet.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Flet extends Instrument{
+    public Flet(String prod, LocalDate rP){
+        super(prod,rP);
+    }
+
+    public String dzwiek(){
+        return "fiu fiuuu";
+    }
+}
